ab9a0d59df933021fcb191a11a0792874ebfda72
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-09-20 07:17:47 +0100

    Add day 18 part 2

--- Day18.scala
+++ Day18.scala
@@ -17,14 +17,21 @@ object Day18 {
   private[this] final val Lumberyard = '#'
 
   private[this] final val Part1Generations = 10
+  private[this] final val Part2Generations = 1000000000
 
   def main(args: Array[String]): Unit = {
     val matrix: Matrix2D = readResourceLines("day18.txt")
       .map(line => line.toCharArray)
 
     lazy val part1 = countMatrixScore(generate(matrix, Part1Generations))
+    lazy val part2 = {
+      val (period, delta0) = calcMatrixPeriod(matrix)
+      val simplifiedNumGenerations = delta0 + ((Part2Generations - delta0) % period)
+      countMatrixScore(generate(matrix, simplifiedNumGenerations))
+    }
 
     println(s"Part 1: $part1")
+    println(s"Part 2: $part2")
   }
 
   def copyMatrix2D(matrix: Matrix2D): Matrix2D = matrix.map(_.map(identity))
@@ -32,6 +39,25 @@ object Day18 {
   def countElement(matrix: Matrix2D, element: Char): Int = matrix.map(_.count(_ == element)).sum
 
   def countMatrixScore(matrix: Matrix2D): Int = countElement(matrix, Tree) * countElement(matrix, Lumberyard)
+
+  def hashMatrix(matrix: Matrix2D): String = matrix.map(_.mkString).mkString
+
+  def calcMatrixPeriod(matrix: Matrix2D): (Int, Int) = {
+    val visited = mutable.Map[String, Int]()
+
+    @tailrec
+    def calcMatrixPeriodInner(m: Matrix2D, generationId: Int): (Int, Int) = {
+      val hash = hashMatrix(m)
+
+      if (visited.contains(hash)) (generationId - visited(hash), visited(hash))
+      else {
+        visited.put(hash, generationId)
+        calcMatrixPeriodInner(generate(m), generationId + 1)
+      }
+    }
+
+    calcMatrixPeriodInner(matrix, 0)
+  }
 
   @tailrec
   def generate(matrix: Matrix2D, numGenerations: Int = 1): Matrix2D = if (numGenerations == 0) matrix else {
