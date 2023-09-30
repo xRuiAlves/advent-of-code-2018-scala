d893ba9a7d62e5ec985cc48f14d8003ff288d494
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-09-30 17:18:32 +0100

    Add day 21 part 2

--- Day21.scala
+++ Day21.scala
@@ -11,6 +11,7 @@ import scala.collection.mutable
 import scala.annotation.tailrec
 
 object Day21 {
+  private[this] final val NumRegisters = 6
   // This is the only instruction in the input program that interacts with register 0!
   private[this] final val Reg0InstructionId = 28
 
@@ -28,26 +29,31 @@ object Day21 {
         case _                           => throw new Error("Invalid input line!")
       }
 
-    def evalInstruction(instruction: Instruction, registers: Array[Int]): Int = instruction.opcode match {
-      case "addi" => registers(instruction.op1) + instruction.op2
-      case "addr" => registers(instruction.op1) + registers(instruction.op2)
-      case "seti" => instruction.op1
-      case "setr" => registers(instruction.op1)
-      case "mulr" => registers(instruction.op1) * registers(instruction.op2)
-      case "muli" => registers(instruction.op1) * instruction.op2
-      case "eqrr" => if (registers(instruction.op1) == registers(instruction.op2)) 1 else 0
-      case "eqir" => if (instruction.op1 == registers(instruction.op2)) 1 else 0
-      case "eqri" => if (registers(instruction.op1) == instruction.op2) 1 else 0
-      case "gtrr" => if (registers(instruction.op1) > registers(instruction.op2)) 1 else 0
-      case "gtir" => if (instruction.op1 > registers(instruction.op2)) 1 else 0
-      case "gtri" => if (registers(instruction.op1) > instruction.op2) 1 else 0
-      case "banr" => registers(instruction.op1) & registers(instruction.op2)
-      case "bani" => registers(instruction.op1) & instruction.op2
-      case "borr" => registers(instruction.op1) | registers(instruction.op2)
-      case "bori" => registers(instruction.op1) | instruction.op2
-    }
+    def evalInstruction(instruction: Instruction, registers: Array[Int]): Int =
+      instruction.opcode match {
+        case "addi" => registers(instruction.op1) + instruction.op2
+        case "addr" => registers(instruction.op1) + registers(instruction.op2)
+        case "seti" => instruction.op1
+        case "setr" => registers(instruction.op1)
+        case "mulr" => registers(instruction.op1) * registers(instruction.op2)
+        case "muli" => registers(instruction.op1) * instruction.op2
+        case "eqrr" => if (registers(instruction.op1) == registers(instruction.op2)) 1 else 0
+        case "eqir" => if (instruction.op1 == registers(instruction.op2)) 1 else 0
+        case "eqri" => if (registers(instruction.op1) == instruction.op2) 1 else 0
+        case "gtrr" => if (registers(instruction.op1) > registers(instruction.op2)) 1 else 0
+        case "gtir" => if (instruction.op1 > registers(instruction.op2)) 1 else 0
+        case "gtri" => if (registers(instruction.op1) > instruction.op2) 1 else 0
+        case "banr" => registers(instruction.op1) & registers(instruction.op2)
+        case "bani" => registers(instruction.op1) & instruction.op2
+        case "borr" => registers(instruction.op1) | registers(instruction.op2)
+        case "bori" => registers(instruction.op1) | instruction.op2
+      }
 
-    def runProgram(registers: Array[Int], ip: Int = 0): Int = {
+    @tailrec
+    def findLowerBound(
+        registers: Array[Int] = Array.fill(NumRegisters)(0),
+        ip: Int = 0
+    ): Int = {
       if (ip >= instructions.length) throw new Error("The program exited!")
       else {
         registers(ipRegister) = ip
@@ -58,11 +64,41 @@ object Day21 {
         }
 
         registers(instruction.op3) = evalInstruction(instruction, registers)
-        runProgram(registers, registers(ipRegister) + 1)
+        findLowerBound(registers, registers(ipRegister) + 1)
       }
     }
 
-    val part1 = runProgram(Array(0, 0, 0, 0, 0, 0))
-    println(s"Part 1: $part1")
+    @tailrec
+    def findUpperBound(
+        registers: Array[Int] = Array.fill(NumRegisters)(0),
+        ip: Int = 0,
+        visited: mutable.Set[Int] = mutable.Set[Int](),
+        previousSpecialRegValue: Int = -1
+    ): Int = {
+      if (ip >= instructions.length) throw new Error("The program exited!")
+      else {
+        registers(ipRegister) = ip
+        val instruction = instructions(ip)
+        registers(instruction.op3) = evalInstruction(instruction, registers)
+
+        if (ip == Reg0InstructionId) {
+          val specialValue = registers(instruction.op1)
+
+          // Check if the value in the special registers repeats. When this happens, the last value that was present in
+          // this special register is the one that causes the program to halt after executing the most instructions!
+          if (visited.contains(specialValue)) {
+            return previousSpecialRegValue
+          }
+
+          visited.addOne(specialValue)
+          findUpperBound(registers, registers(ipRegister) + 1, visited, specialValue)
+        } else {
+          findUpperBound(registers, registers(ipRegister) + 1, visited, previousSpecialRegValue)
+        }
+      }
+    }
+
+    println(s"Part 1: ${findLowerBound()}")
+    println(s"Part 2: ${findUpperBound()}")
   }
 }
